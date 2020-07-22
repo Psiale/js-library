37096427b8cca6c290e49bfe40e42edba9d13cdb
Rory Hellier <rory.hellier@googlemail.com>
2020-07-22 23:52:34 +0100

    Rewrote the Render Function and Added Some CSS Styling
     - Commented out onld render code
     - Added new render function which iterates over the library array and returns each entry in its 'info' format, appends it to a new <p> tag
     - Added main page container and centered the form and the books display
    
    REMINDER: The new render function only renders the original library. It needs a conditional to replace the library shown when the page is first loaded with the new, updated library.

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-const ourLibrary = [
-  new Book('One Hundred Years Of Solitude', 'Gabriel Garcia Marquez', 200, 'Read'),
-  new Book('Clash of Kings', 'George R.R. Martin', 1000, 'Read'),
-  new Book('Bible', 'God', 100, 'Read'),
-  new Book('I Robot', 'Isaac Asimov', 300),
-];
-
 function Book(title, author, pageCount, readStatus = 'not read') {
   this.title = title;
   this.author = author;
@@ -16,14 +9,30 @@ Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pageCount} pages, ${this.readStatus}.`;
 };
 
-function render (updatedLibrary) {
-    for (const books in updatedLibrary) {
-        let el = document.createElement('p')
-      let text = document.createTextNode(`${updatedLibrary[books].info()}`);
-      el.appendChild(text)
-      result.appendChild(el);
-    }
-  
+const ourLibrary = [
+  new Book('One Hundred Years Of Solitude', 'Gabriel Garcia Marquez', 200, 'Read'),
+  new Book('Clash of Kings', 'George R.R. Martin', 1000, 'Read'),
+  new Book('Bible', 'God', 100, 'Read'),
+  new Book('I Robot', 'Isaac Asimov', 300),
+];
+
+/*function render (updatedLibrary) {
+  for (const books in updatedLibrary) {
+    let el = document.createElement('p')
+    let text = document.createTextNode(`${updatedLibrary[books].info()}`);
+    el.appendChild(text)
+    result.appendChild(el);
+  }
+};*/
+
+function render(library) {
+  for (let i = 0; i < library.length; i++) {
+    let para = document.createElement('p');
+    let content = document.createTextNode(`${library[i].info()}`);
+    para.appendChild(content);
+    let newBook = document.getElementById('result');
+    newBook.appendChild(para);
+  }
 }
 
 function addBookToLibrary(title, author, pageCount, readStatus = 'not read') {
@@ -37,7 +46,7 @@ function addBookToLibrary(title, author, pageCount, readStatus = 'not read') {
   const result = document.getElementById('result');
   // let text = document.createTextNode(`${ourLibrary.length}`);
   // result.appendChild(text);
-  result.replaceChild(ourLibrary, updatedLibrary);
+  //result.replaceChild(ourLibrary, updatedLibrary);
 }
 
 
