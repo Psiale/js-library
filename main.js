9390ef2f875c26f497a5b67b663c6b33bd505d93
Alexis <psialedev@gmail.com>
2020-07-22 16:04:30 -0500

    Created inputs for the creation of the book, render array length to test the function

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
-const ourLibrary = [];
+const ourLibrary = [
+    new Book('One Hundred Years Of Solitude', 'Gabriel Garcia Marquez', 200, 'Read'),
+    new Book('Clash of Kings', 'George R.R. Martin', 1000, 'Read'),
+    new Book('Bible', 'God', 100, 'Read'),
+    new Book('I Robot', 'Isaac Asimov', 300),
+];
 
 function Book(title, author, pageCount, readStatus = 'not read') {
   this.title = title;
   this.author = author;
   this.pageCount = pageCount;
   this.readStatus = readStatus;
-};
+}
 
 Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pageCount} pages, ${this.readStatus}.`;
 };
 
 function addBookToLibrary(title, author, pageCount, readStatus = 'not read') {
-  let title = document.getElementById('title-input').value;
-  let author = document.getElementById('author-input').value;
-  let pageCount = document.getElementById('pageCount-input').value;
-  let readStatus = document.getElementById('readStatus-input').value;
+  title = document.getElementById('title-input').value;
+  author = document.getElementById('author-input').value;
+  pageCount = document.getElementById('pageCount-input').value;
+  readStatus = document.getElementById('readStatus-input').value;
   let newEntry = new Book(title, author, pageCount, readStatus);
   let updatedLibrary = ourLibrary;
   updatedLibrary.push(newEntry);
+  let result = document.getElementById('result');
+
+ let text = document.createTextNode(`${ourLibrary.length}`);
+    result.appendChild(text);
   return updatedLibrary;
-};
-
-
+}
